bc933f736874c78427bcf03a3833b289d7447dcd
Rain <therainy.dev@tutanota.de>
2024-03-18 19:19:58 +0000

    just do a randomization in setwlmac.nix

--- services/setwlmac.nix
+++ services/setwlmac.nix
@@ -6,13 +6,10 @@
 		script = ''
 			export PATH=/run/current-system/sw/bin
 			ip link set dev wlp0s20f3 down
-			#ip link set dev wlp0s20f3 address 02:$(
-			#	xxd -p -l 5 /dev/random |
-			ip link set dev wlp0s20f3 address 12:00:$(
-				printf "%x" $(date +%s) |
+			ip link set dev wlp0s20f3 address 02:$(
+				xxd -p -l 5 /dev/random |
 				sed 's/../&:/g; s/:$//'
 			)
-			#ip link set dev wlp0s20f3 address 22:00:00:00:00:00
 			ip link set dev wlp0s20f3 up
 		'';
 	};
