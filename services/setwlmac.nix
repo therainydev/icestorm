55a7f6b1207ef3f41fdc64424e1b18370f828d20
Rain <therainy.dev@tutanota.de>
2024-03-18 19:28:49 +0000

    use packages in setwlmac.nix
    do not assume presence of utilities in /run/sw/current-system/bin

--- services/setwlmac.nix
+++ services/setwlmac.nix
@@ -1,16 +1,15 @@
-{ config, ... }: {
+{ config, pkgs, ... }: {
 	systemd.services.setwlmac = {
 		description = "Set MAC Address";
 		wantedBy    = [ "wpa_supplicant.service" ];
 		after       = [ "wpa_supplicant.service" ];
 		script = ''
-			export PATH=/run/current-system/sw/bin
-			ip link set dev wlp0s20f3 down
-			ip link set dev wlp0s20f3 address 02:$(
-				xxd -p -l 5 /dev/random |
-				sed 's/../&:/g; s/:$//'
+			${pkgs.iproute2}/bin/ip link set dev wlp0s20f3 down
+			${pkgs.iproute2}/bin/ip link set dev wlp0s20f3 address 02:$(
+				${pkgs.unixtools.xxd}/bin/xxd -p -l 5 /dev/random |
+				${pkgs.gnused}/bin/sed 's/../&:/g; s/:$//'
 			)
-			ip link set dev wlp0s20f3 up
+			${pkgs.iproute2}/bin/ip link set dev wlp0s20f3 up
 		'';
 	};
 }
